c95095ca91beec583a5d5473b8f5672c4c1d2629
Hung Tran <tmhung88@outlook.com>
2019-10-22 18:58:06 +0700

    Added MaterialUI

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,7 +1,8 @@
 describe('[App]', () => {
   it('[Home Page]', () => {
-    cy.step('A link to the offical React website')
+    cy.step('Dashboard is loaded')
     cy.visit('/')
-    cy.get('a[target=_blank]').contains('Learn React')
+    cy.get('h1').contains('Dashboard')
+    cy.get('a').contains('Your Website')
   })
 })
