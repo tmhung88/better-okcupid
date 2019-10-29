0836b5578e24a5c133d5068de26251cc15cc76c5
Hung Tran <tmhung88@outlook.com>
2019-10-29 21:38:00 +0700

    Add an user with their profile link

--- src/components/userBookmark.tsx
+++ src/components/userBookmark.tsx
@@ -1,24 +1,46 @@
-import React, { Fragment, FunctionComponent, useState } from 'react'
-import { Box, Button, Paper, TextField } from '@material-ui/core'
+import React, { FunctionComponent, useState } from 'react'
+import { Button, Paper, TextField, Typography } from '@material-ui/core'
 import { botOkcService, Profile } from '../okc/okcService'
-import { UserCard } from './card'
 import { userBookmarkService } from '../services/bookmarkService'
+import { isEmpty } from '../services/utils'
+import { UserCard } from './card'
 
 type Props = {
   onAdd: (profile: Profile) => void
 }
 export const UserBookmark: FunctionComponent<Props> = ({ onAdd }: Props) => {
-  const [userId, setUserId] = useState<string>('')
+  const [profileLink, setProfileLink] = useState<string>('')
   const [profile, setProfile] = useState<Profile | null>(null)
-  const handleOnUserIdChanged = (userId: string) => {
-    setUserId(userId)
-    if (userId.length === 0) {
+  const [error, setError] = useState<string | null>(null)
+
+  const extractUserId = (link: string): string | null => {
+    const PREFIX = '.com/profile/'
+    const SUFFIX = '?'
+    const userIdStart = link.includes(PREFIX) ? link.indexOf(PREFIX) + PREFIX.length : -1
+    if (userIdStart == -1) {
+      return null
+    }
+    const userIdEnd = link.includes(SUFFIX) ? link.lastIndexOf(SUFFIX) : link.length
+    return link.substring(userIdStart, userIdEnd)
+  }
+  const handleOnUserIdChanged = (updatedProfileLink: string) => {
+    setProfileLink(updatedProfileLink)
+    if (isEmpty(updatedProfileLink)) {
       setProfile(null)
+      return
+    }
+
+    const userId = extractUserId(updatedProfileLink)
+    if (userId === null) {
+      setError(
+        'Profile link is invalid. Valid ones look like https://www.okcupid.com/profile/robot , https://www.okcupid.com/profile/589275343647',
+      )
       return
     }
 
     botOkcService.getProfile(userId).then(profile => {
       setProfile(profile)
+      setError(null)
     })
   }
   const handleOnAddClick = (profile: Profile | null) => {
@@ -27,15 +49,15 @@ export const UserBookmark: FunctionComponent<Props> = ({ onAdd }: Props) => {
     }
     userBookmarkService.bookmark(profile.userId)
     onAdd(profile)
-    setUserId('')
+    setProfileLink('')
     setProfile(null)
   }
 
   return (
     <Paper>
       <TextField
-        label="User ID"
-        value={userId}
+        label="Profile Link"
+        value={profileLink}
         onChange={({ target }): void => handleOnUserIdChanged(target.value)}
         margin="normal"
       />
@@ -43,6 +65,11 @@ export const UserBookmark: FunctionComponent<Props> = ({ onAdd }: Props) => {
       <Button variant="contained" color="primary" disabled={!profile} onClick={(): void => handleOnAddClick(profile)}>
         Add
       </Button>
+      {error && (
+        <Typography color="error" component="p">
+          {error}
+        </Typography>
+      )}
       {profile && <UserCard profile={profile} />}
     </Paper>
   )
